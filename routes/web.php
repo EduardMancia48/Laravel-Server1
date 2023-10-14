f62f6d3a8eb22946c618d5fc91c47a6dd8a99152
Eduardo Mancia <40743410+EduardMancia48@users.noreply.github.com>
2023-10-13 21:29:20 -0600

    Pagina del server1

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/server1', function () {
+    return 'Serve1';
+});
